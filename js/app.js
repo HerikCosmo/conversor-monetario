a04ef1b486193c1c91dd0061f4133ea6a832683e
Herik Martins <herikcosmo.m@gmail.com>
2021-08-04 21:11:15 -0300

    logica feita

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
-const input1 = document.querySelector('#valor-1')
-const input2 = document.querySelector('#valor-2')
+const inputs = document.querySelectorAll('.inputs')
 const descricao = document.querySelector('#descricao')
 const moedas = document.querySelectorAll(".moedas")
-const select = document.querySelector("#moeda1")
-const select2 = document.querySelector("#moeda2")
+const [select, select2] = moedas
+
+window.addEventListener('load', () => {
+    buscarMoeda('USD', 'BRL')
+    listarMoedas()
+    moedasDisponiveis()
+
+    select.addEventListener("change", () => moedasDisponiveis())
+    moedas.forEach(moeda => moeda.addEventListener('change', () => capturaValor()))
+})
 
 function buscarMoeda(code, codein){
     fetch(`https://economia.awesomeapi.com.br/last/${code}-${codein}`)
@@ -16,32 +23,29 @@ function buscarMoeda(code, codein){
         return {cotacao: data[`${code}${codein}`].ask, desc: data[`${code}${codein}`].name}
     })
     .then(obj => mostrar(obj.cotacao, obj.desc))
-    .catch(e => {
-        throw Error(e)
-})
+    .catch(e => { throw Error(e) })
 }
 
 function mostrar(cotacao, desc){
     const nomesMoeda = desc.split('/')
     descricao.innerText = `1 ${nomesMoeda[0]} igual a ${cotacao} ${nomesMoeda[1]}`
-    atualizaValor(cotacao)
+    atualizaValor(cotacao, inputs[0], inputs[1])
+    inputs[0].addEventListener("keyup", () => atualizaValor(cotacao, inputs[0], inputs[1]))
+    inputs[1].addEventListener("keyup", () => atualizaValor(cotacao, inputs[1], inputs[0], true))
 }
 
-function atualizaValor(cotacao){
-    input1.addEventListener("keyup", () => {
-        const valor1 = parseFloat(input1.value)
-        if(isNaN(valor1)) input2.value = ''
+function atualizaValor(cotacao, i1, i2, invertido = false){
+        const valor1 = parseFloat(i1.value)
+        if(isNaN(valor1)) i2.value = ''
         else{
             const valor2 = parseFloat(cotacao)
-            const convertido = (valor1 * valor2).toFixed(2)
-            input2.value = convertido    
+            i2.value = !invertido ? (valor1 * valor2).toFixed(2) : (valor1 / valor2).toFixed(2)
         }        
-    })
 }
 
 function capturaValor(){
-    const moeda1 = moedas[0].value
-    const moeda2 = moedas[1].value
+    const moeda1 = select.value
+    const moeda2 = select2.value
     buscarMoeda(moeda1, moeda2)
 }
 
@@ -57,12 +61,3 @@ function moedasDisponiveis(){
         select2.innerHTML += `<option value='${comb.codein}'>${comb.name}</option>`
     })
 }
-
-window.addEventListener('load', () => {
-    buscarMoeda('USD', 'BRL')
-    listarMoedas()
-    moedasDisponiveis()
-
-    select.addEventListener("change", () => moedasDisponiveis())
-    moedas.forEach(moeda => moeda.addEventListener('change', () => capturaValor()))
-})
